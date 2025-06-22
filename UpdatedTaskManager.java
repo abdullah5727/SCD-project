33bd028e4bbb4c35736f3284b5514619ee9b1cbf
abdul-ahad656 <adibutt457@gmail.com>
2025-06-22 23:31:27 +0500

    Create UpdatedTaskManager.java

---
+++ UpdatedTaskManager.java
@@ -0,0 +1,41 @@
+package ToDoApp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TaskManager {
+    private ArrayList<Task> taskList;
+
+    public TaskManager() {
+        taskList = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+        scheduleReminder(task);
+    }
+
+    public void deleteTask(int index) {
+        if (index >= 0 && index < taskList.size()) {
+            taskList.remove(index);
+        }
+    }
+
+    public void markAsCompleted(int index) {
+        if (index >= 0 && index < taskList.size()) {
+            taskList.get(index).markAsCompleted();
+        }
+    }
+
+    public ArrayList<Task> getTasks() {
+        return taskList;
+    }
+
+    public void sortTasksByDueDate() {
+        Collections.sort(taskList, Comparator.comparing(Task::getDueDate));
+    }
+
+    public void scheduleReminder(Task task) {
+        System.out.println("Reminder scheduled for: " + task.getTitle());
+    }
+}
